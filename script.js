261b3aa9b271bfa3639403c25dce3bd1d29d4595
me2uuuu <110250077+me2uuuu@users.noreply.github.com>
2025-03-08 23:41:32 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("neonBeamBtn").addEventListener("click", function() {
+    document.body.style.background = "radial-gradient(circle, rgba(0,0,255,1) 0%, rgba(0,0,0,1) 100%)";
+    alert("⚡ 네온빔 충전 완료! ⚡");
+});
